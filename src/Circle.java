f96f942d996ddb602bc2b88c5cdf73849e7187c0
burkhanovaakmaral <121574995+burkhanovaakmaral@users.noreply.github.com>
2023-01-23 13:23:17 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Circle {
+    private static final double PI = 3.14;
+
+    public static double area(double radius) {
+        return  PI* (radius * radius);
+
+    }
+
+    public static double circumference(double radius) {
+        return  PI* 2 * radius;
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double rad =  5.5;
+        System.out.println(Circle.area(rad));
+        System.out.println(Circle.circumference(rad));
+
+
+
+    }
+}
